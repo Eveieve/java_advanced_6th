d9ad893c8c9539d4b8036ceb5f300d57a5cb6ada
thatzcool <mycaptain0771@gmail.com>
2025-02-07 10:32:37 +0900

    day05_01

---
+++ src/innerClass/ex01/ABMain.java
@@ -0,0 +1,10 @@
+package innerClass.ex01;
+
+public class ABMain {
+    public static void main(String[] args) {
+        //A객체 생성
+        A a = new A();
+        //B 객체 생성
+        A.B b = a.new B();
+    }
+}
