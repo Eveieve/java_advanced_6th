fada8e571b4512fd821b505abeea3a10f494f822
thatzcool <mycaptain0771@gmail.com>
2025-02-10 11:24:44 +0900

    day06_03

---
+++ src/api/UserMain.java
@@ -0,0 +1,30 @@
+package api;
+
+public class UserMain {
+    public static void main(String[] args) {
+
+        //얕은 복사(shallow copy)
+        User user = new User();
+        user.setName("Shin");
+        User copy = user;
+
+        System.out.println(user.hashCode());
+        System.out.println(copy.hashCode());
+        System.out.println(user.equals(copy));
+
+        //깊은 복사(deep copy)
+        try {
+            User user1 = new User();
+            user1.setName("Shin");
+            User copy1 =  (User)user1.clone();
+
+            System.out.println(user1.hashCode());
+            System.out.println(copy1.hashCode());
+            System.out.println(user1.equals(copy1));
+
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
