dbbba89f8de434cf458ede19c97552ce815dcf93
thatzcool <mycaptain0771@gmail.com>
2025-02-10 11:51:14 +0900

    day06_04

--- src/api/User.java
+++ src/api/User.java
@@ -1,12 +1,25 @@
 package api;
 //객체복사 메소드를 사용하기 위해서는 Cloneable 인터페이스를 구현하여 clone() 재정의하여 사용한다.
 public class User implements Cloneable{
+    private  int id;
     private  String name;
 
-    public void setName(String name) {
+    public User(int id, String name) {
+        this.id = id;
         this.name = name;
     }
-    public Object clone() throws CloneNotSupportedException{
-        return super.clone();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+        public void setName(String name) {
+        this.name = name;
+    }
+    public User clone() throws CloneNotSupportedException{
+        return (User) super.clone();
     }
 }
