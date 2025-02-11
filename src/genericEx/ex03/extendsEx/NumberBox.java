805ae336a45d8c0f1e42ceb962ad2a7de268d285
thatzcool <mycaptain0771@gmail.com>
2025-02-11 11:51:22 +0900

    day07_03

--- src/genericEx/ex03/extendsEx/NumberBox.java
+++ src/genericEx/ex03/extendsEx/NumberBox.java
@@ -13,12 +13,23 @@ public class NumberBox<T extends Number> {
     public double doubleValue(){
         return number.doubleValue();
     }
-
+// 상한 제한 와일드카드 (? extends T) 알려지지 않은 유형이 특정 타입 또는 그 하위클래스가 되도록 제한
     public void printNumberList(List<? extends Number> list){
        for(Number number : list){
            System.out.println(number);
        }
 
         }
+// 어떤 유형과도 함께 사용할 수 있도록 유연성 제공
+    public void printList(List<?> list){
+        for(Object element : list) System.out.println(element);
     }
-}
+
+    //하한 제한 와일드 카드 (? super T) 특정 타입 또는 그 상위 클래스 중 하나를 허용
+    public void addNumbers(List<? super Integer> list){
+        list.add(10);
+    }
+
+    }
+
+
---
+++ src/genericEx/pratices/GennericMethodEx.java
@@ -0,0 +1,4 @@
+package genericEx.pratices;
+//어떤 타입의 배열을 받아 요소를 출력하는 제너릭 메서드 printArray를 작성하세요
+public class GennericMethodEx {
+}
