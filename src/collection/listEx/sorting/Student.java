ccc570c8732f1bf0432ed801134673a557910f01
thatzcool <mycaptain0771@gmail.com>
2025-02-12 11:19:39 +0900

    day08_03

---
+++ src/collection/listEx/sorting/Student.java
@@ -0,0 +1,20 @@
+package collection.listEx.sorting;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student implements Comparable<Student>{
+
+       private int sno;   //학번
+       private String name;
+
+    @Override
+    public int compareTo(Student o) {
+        if (this.sno > o.sno) return 1;
+        else if (this.sno == o.sno) return 0;
+        else return -1;
+        //return o.sno - this.sno;
+    }
+}
