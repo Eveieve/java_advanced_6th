574a3e32a3ff9c1fc256cb04c0c39503dd25374c
thatzcool <mycaptain0771@gmail.com>
2025-02-13 09:21:43 +0900

    day09_1

--- src/workshop/studentWorkshop/Student.java
+++ src/workshop/studentWorkshop/Student.java
@@ -1,4 +1,4 @@
-package workshop;
+package workshop.studentWorkshop;
 
 import java.io.Serializable;
 
--- src/workshop/studentWorkshop/StudentInput.java
+++ src/workshop/studentWorkshop/StudentInput.java
@@ -1,4 +1,4 @@
-package workshop;
+package workshop.studentWorkshop;
 
 import java.io.BufferedReader;
 import java.io.File;
