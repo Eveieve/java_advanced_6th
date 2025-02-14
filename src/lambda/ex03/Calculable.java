0e8a7e0a76d691c08a52066afe1ac1bfd1e7be24
thatzcool <mycaptain0771@gmail.com>
2025-02-14 12:04:38 +0900

    day10_04

---
+++ src/lambda/ex03/Calculable.java
@@ -0,0 +1,7 @@
+package lambda.ex03;
+
+@FunctionalInterface
+public interface Calculable {
+    //추상 메소드
+    double calculate(int x,int y);
+}
---
+++ src/lambda/ex03/Main.java
@@ -0,0 +1,18 @@
+package lambda.ex03;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person person = new Person();
+
+
+    }
+
+
+    public static double sum(double x, double y) {
+        return (x,y);
+    }
+}
+
+
+
