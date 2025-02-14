7821018f38cc172775d50450f199fabe0f89105f
thatzcool <mycaptain0771@gmail.com>
2025-02-14 12:46:45 +0900

    day10_08

---
+++ src/lambda/ex06/Creatable1.java
@@ -0,0 +1,5 @@
+package lambda.ex06;
+@FunctionalInterface
+public interface Creatable1 {
+    public Member create(String id);
+}
---
+++ src/lambda/ex06/Creatable2.java
@@ -0,0 +1,5 @@
+package lambda.ex06;
+@FunctionalInterface
+public interface Creatable2 {
+    public Member create(String id,String name);
+}
