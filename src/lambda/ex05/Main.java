aabae5fd549ad049c521f51bb8db14e998fd3e51
thatzcool <mycaptain0771@gmail.com>
2025-02-14 12:33:55 +0900

    day10_07

---
+++ src/lambda/ex05/Main.java
@@ -0,0 +1,10 @@
+package lambda.ex05;
+
+public class Main {
+
+    public static void main(String[] args) {
+          Person person = new Person();
+          person.ordering(String::compareToIgnoreCase);
+        //person.ordering((a,b) ->a.compareToIgnoreCase(b));
+    }
+}
