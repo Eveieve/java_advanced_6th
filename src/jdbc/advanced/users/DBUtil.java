d50f559029a45a47bb6de324c3f7fb89d2d3671f
thatzcool <mycaptain0771@gmail.com>
2025-02-28 09:41:29 +0900

    jdbc_pratice

---
+++ src/jdbc/advanced/users/DBUtil.java
@@ -0,0 +1,15 @@
+package jdbc.advanced.users;
+
+/**
+ * packageName   : jdbc.advanced.users
+ * fileName      : DBUtil
+ * author        : a
+ * date          : 2025-02-28
+ * description   : 데이터베이스 연결 작업 클래스
+ * =================================================
+ * DATE             AUTHOR             NOTE
+ * -------------------------------------------------
+ * 2025-02-28        a
+ */
+public class DBUtil {
+}
